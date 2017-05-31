1489d5832fe44601afeb08e27e58a5f76b0b5007
gada1982 <daniel@gattringer.biz>
2017-05-31 16:31:15 +0200

    First version implemented

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,6 @@
 #include "PID.h"
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -6,16 +8,39 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+  d_error = 0;
+  p_error = 0;
+  i_error = 0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  double steer;
+  
+  steer = -Kp*p_error - Kd*d_error - Ki*i_error;
+  
+  if (fabs(steer)>1.0){
+    if (steer > 0){
+      steer = 1.0;
+    } else {
+      steer = -1.0;
+    }
+  }
+  
+  return steer;
 }
 
